a1d2369a6801e2fab01ac3f7172182a4c204f41b
Syahrin Suhaimi <60390413+gurzugon@users.noreply.github.com>
2021-03-25 16:56:06 +0800

    Initial Commit

--- forms.php
+++ forms.php
@@ -15,7 +15,6 @@ if($result_admin = mysqli_query($conn, $sql_admin))
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
