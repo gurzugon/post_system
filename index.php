9a6c09634df31e253018dae64817d83ea5382bed
Syahrin Suhaimi <60390413+gurzugon@users.noreply.github.com>
2021-07-16 08:35:02 +0800

    Initial commit

--- index.php
+++ index.php
@@ -47,7 +47,6 @@ if (isset($_POST['login']))
     <!-- Custom Fonts -->
     <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
     <div class="container">
         <div class="row">
