93981469dbffd019fcddd2b5bd675efd3d2109d0
Syahrin Suhaimi <60390413+gurzugon@users.noreply.github.com>
2021-07-18 13:05:08 +0800

    Initial commit

--- forms.php
+++ forms.php
@@ -44,7 +44,6 @@ if($result_admin = mysqli_query($conn, $sql_admin))
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
 <body>
